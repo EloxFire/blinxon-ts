9a330bdb0d55e412d001768a06a24a6887eff95f
Enzo Avagliano <enzo.avagliano@ynov.com>
2023-02-03 18:17:12 +0100

    Updated header with title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <div id="home">
       <div className="header">
+        <div className="title-container">
+          <h1>Blinxon</h1>
+          <h2>4L trophy 2023</h2>
+        </div>
       </div>
     </div>
   )
