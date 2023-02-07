958431886e107037685410602208573dc7cf59cf
Enzo Avagliano <enzo.avagliano@ynov.com>
2023-02-07 01:29:45 +0100

    Wip on sposnors

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Raid from './pages/Raid';
+import Sponsors from './pages/Sponsors';
 import './styles/globals.scss'
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={<Home changeActive={setActive} />} />
         <Route path="/a-propos" element={<About changeActive={setActive} />} />
         <Route path="/raid" element={<Raid changeActive={setActive} />} />
+        <Route path="/sponsors" element={<Sponsors changeActive={setActive} />} />
         <Route path="*" element={<NotFound changeActive={setActive} />} />
       </Switch>
       <Footer />
--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
-export default function Sponsors() {
+interface SponsorsProps {
+  changeActive: (active: number) => void
+}
+
+export default function Sponsors({ changeActive }: SponsorsProps) {
   return (
-    <div>Sponsors</div>
+    <div id='sponsors'>
+      <div className="header">
+        <div className="title-container">
+          <h1>Nos sponsors</h1>
+          <h2>Merci à eux !</h2>
+        </div>
+      </div>
+      <div className="content">
+        <h1>étapes clés du 4L Trophy</h1>
+        <p>Voici la liste détaillée de toutes les étapes clés par lesquels nous passerons lors de notre aventure au 4L Trophy :</p>
+      </div>
+    </div>
   )
 }
